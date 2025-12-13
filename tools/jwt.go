02d915a2ace5883d073983e40338deb6394290a5
xor27 <129921275+xor27@users.noreply.github.com>
2025-12-13 16:33:59 +0700

    use JWT_SECRET from env

--- tools/jwt.go
+++ tools/jwt.go
@@ -1,12 +1,11 @@
 package tools
 
 import (
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
-
-var jwtKey = []byte("SECRET_KEY")
 
 func GenerateJWT(userID int, role string) (string, error) {
 	claims := jwt.MapClaims{
@@ -16,5 +15,9 @@ func GenerateJWT(userID int, role string) (string, error) {
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// ambil secret dari env variable JWT_SECRET
+	jwtKey := []byte(os.Getenv("JWT_SECRET"))
+
 	return token.SignedString(jwtKey)
 }
